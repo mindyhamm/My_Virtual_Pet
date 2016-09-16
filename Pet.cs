c7af4656e55996bb57633645d65277d9c6df11dd
mindy <mindyhammock@gmail.com>
2016-09-16 16:13:49 -0400

    first commit

---
+++ Pet.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Virtual_Pet
+{
+    class Pet //main method
+    {
+        private string num1;
+        private string num2;
+        private string num3;
+        private int one;
+
+        public Pet(string num1)
+        {
+            this.num1 = num1;
+        }    
+        public Pet(string num1, string num2)//properties
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        } 
+        public Pet (string num1, string num2, int one)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+        public void //tick
+        {
+        for (int x = 0; x < 10; x++)
+            Console.Writeline();
+
+        }
+        public class Pet()
+        {
+    
+        }
+        public class Pet 
+        {
+
+        }
+        public class pet 
+        {
+
+        }
+             
+}   
